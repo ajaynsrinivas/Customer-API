c472733a30e98c925e5bce4216efc9be4a8e2c24
Ajay <ajaynsrinivas@gmail.com>
2018-02-20 15:56:04 +0530

    set up for heroku deployment

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-var db = mongoose.connect('mongodb://localhost:27017/CustomerInfo',{
+var db = mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/CustomerInfo',{
     useMongoClient: true
 });
 
